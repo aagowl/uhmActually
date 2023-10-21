782fb15a7f9f19f996156195bcb3cd3ec03bade4
aagowl <abigail.gatsch@gmail.com>
2023-10-21 17:01:14 -0600

    activate client

--- index.js
+++ index.js
@@ -1,3 +1,12 @@
 const { Client, Events, GatewayIntentBits } = require('discord.js');
 const { token } = require("./config.json");
 
+const client = new Client({ intents: [GatewayIntentBits.Guilds] });
+
+// run when client is ready
+client.once(Events.ClientReady, c => {
+    console.log(`Bot is alive and logged in as ${c.user.tag}`);
+});
+
+// Log in
+client.login(token);
