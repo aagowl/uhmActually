b11fc639d7d5fe210ba43faa9c6b27608dc3a034
aagowl <abigail.gatsch@gmail.com>
2023-10-21 19:23:19 -0600

    Okay at this point it would be impressive if i actually finished this in 13 hours

--- index.js
+++ index.js
@@ -18,15 +18,39 @@ client.once(Events.ClientReady, c => {
 // Log in
 client.login(token);
 
-// Command handeling
+// Command handling
 for (const file of commandFiles) {
     const filePath = path.join(commandsPath, file);
     const command = require(filePath);
     
-    // checks to ensure that there is data that is ab
+    // checks to ensure that there is proper information to run
     if ('data' in command && 'execute' in command) {
 		client.commands.set(command.data.name, command);
 	} else {
 		console.log(`[WARNING] The command at ${filePath} is missing a required "data" or "execute" property.`);
 	}
 }
+
+client.on(Events.InteractionCreate, async interaction => {
+    if (!interaction.isChatInputCommand()) return;
+        console.log(interaction);
+
+        const command = interaction.client.commands.get(interaction.commandName);
+
+        // does that command even exist?
+        if (!command) {
+            console.error(`No command matching ${interaction.commandName} was found.`);
+            return;
+	}
+
+	try {
+		await command.execute(interaction);
+	} catch (error) {
+		console.error(error);
+		if (interaction.replied || interaction.deferred) {
+			await interaction.followUp({ content: 'There was an error while executing this command!', ephemeral: true });
+		} else {
+			await interaction.reply({ content: 'There was an error while executing this command!', ephemeral: true });
+		}
+	}
+});
