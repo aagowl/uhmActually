7ee65d90fecd3cd6b4310094b52d271120b64ec2
aagowl <abigail.gatsch@gmail.com>
2023-10-21 18:14:56 -0600

    ping command

--- commands/ping.js
+++ commands/ping.js
@@ -3,7 +3,7 @@ const { SlashCommandBuilder } = require('discord.js');
 module.exports = {
     data: new SlashCommandBuilder()
         .setname('ping')
-        .setDescription('for every ping there shall be a pong');
+        .setDescription('for every ping there shall be a pong'),
 
     async execute(interaction) {
         await interaction.reply('Pong!');
